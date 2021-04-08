c757f8af24aefd7a71a858392f1f102d35aaf168
Esteban Burgos <estebanburgos85@gmail.com>
2021-04-08 18:47:58 -0300

    Added DB config

--- database/config/config.js
+++ database/config/config.js
@@ -30,10 +30,14 @@ module.exports = {
         dialect: "mysql",
     },
     production: {
-        username: "root",
-        password: null,
-        database: "database_production",
-        host: "127.0.0.1",
-        dialect: "mysql",
+        username: USERNAME,
+        password: PASSWORD,
+        database: DATABASE,
+        host: HOST,
+        dialect: DIALECT,
+        port: DB_PORT,
+        define: {
+            //"underscored": true,
+        },
     },
 };
